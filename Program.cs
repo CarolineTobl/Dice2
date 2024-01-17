52ea45791c6dcef49b9ab5363e9a4c00b0fcd82c
carolinetobler <carolinetobler@CAROLINETOBBECA>
2024-01-17 13:14:28 -0700

    New Code! With updated comments.

--- Program.cs
+++ Program.cs
@@ -1,47 +1,59 @@
 ﻿using System;
 
-public class DiceSimulator //Create a new Public Class 
+//Second Class Dice Simulator 
+public class DiceSimulator
 {
-    public static int[] RollDice(int numberOfRolls) //public static method named RollDice in the DiceSimulator class. It takes one integer parameter numberOfRolls and returns an array of integers.
+    public static int[] RollDice(int numberOfRolls)
     {
-        int[] rollResults = new int[13]; // Array indices that the numbers 2-12 will be used
+        int[] rollResults = new int[13]; // Array indices 2-12 will be used
 
-        Random rand = new Random(); //Create the Object for where the random numbers will be generated
-        for (int i = 0; i < numberOfRolls; i++) // For loop iterates through rolling the die each time.
+        Random rand = new Random();
+        for (int i = 0; i < numberOfRolls; i++)
         {
-            int roll = rand.Next(1, 7) + rand.Next(1, 7); // Roll the two dice for numbers between 1-6
-            rollResults[roll]++; //Add the two Number of Die to eachother.
+            // Roll two dice for numbers between 1-6 and add the results
+            int roll = rand.Next(1, 7) + rand.Next(1, 7);
+            rollResults[roll]++;
         }
 
-        return rollResults; // return the added together number
+        return rollResults; // Return the roll results array
     }
 }
 
-class Program //new class called Program
+//Main Program Class 
+class Program
 {
-    static void Main(string[] args) //does not return anything
+    static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the dice throwing simulator!"); //Write to Console.
-        Console.Write("How many times would you like to roll 2 6-sided dice? "); // Write to Console
-        int numberOfRolls = int.Parse(Console.ReadLine()); //Take the user input and convert to an integer and store to numberOfRolls variable
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many times would you like to roll 2 6-sided dice? ");
 
-        int[] results = DiceSimulator.RollDice(numberOfRolls); //Call DiceSimulator and RollDice Method
-
-        Console.WriteLine("DICE ROLLING SIMULATION RESULTS"); //Write to Console
-        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls."); //Write to Console
-        Console.WriteLine($"Total number of rolls = {numberOfRolls}."); //Write to Console and print numberOfRolls Variable
-
-        for (int i = 2; i <= 12; i++) //Start iterating through all the possible dice roll sums. 2-12
+        // Attempt to parse the input. If it fails, prompt the user again.
+        if (!int.TryParse(Console.ReadLine(), out int numberOfRolls))
         {
-            int percentage = (results[i] * 100) / numberOfRolls; //Take the sum and calculate the percentage that that sum happened out of the number 
+            Console.WriteLine("Invalid input. Please enter an integer.");
+            return; // Exit if input is invalid
+        }
+
+        // Now we have a valid number of rolls, we can call the RollDice method
+        int[] results = DiceSimulator.RollDice(numberOfRolls);
+
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
+
+        //loop through each sum and calculate the percent occurance 
+        for (int i = 2; i <= 12; i++)
+        {
+            // Calculate the percentage that each sum occurred
+            int percentage = (results[i] * 100) / numberOfRolls;
             Console.Write($"{i}: ");
             for (int j = 0; j < percentage; j++)
             {
-                Console.Write("*"); // Depending on the percent, show that number of astricks.
+                Console.Write("*"); // Print asterisks based on the percentage
             }
             Console.WriteLine();
         }
 
-        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!"); //Write to Console Log
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
     }
 }
